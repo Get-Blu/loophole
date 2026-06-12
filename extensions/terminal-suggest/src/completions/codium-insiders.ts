c3323070e03594e1f2308077f5032c78cd68a720
GarvAgnihotri <garvagnihotri22@gmail.com>
2026-06-12 09:27:46 +0530

    base: VS Code 1.121 clean

---
+++ extensions/terminal-suggest/src/completions/codium-insiders.ts
@@ -0,0 +1,9 @@
+import code from './code';
+
+const codiumInsidersCompletionSpec: Fig.Spec = {
+	...code,
+	name: 'loophole-insiders',
+	description: 'Loophole Insiders',
+};
+
+export default codiumInsidersCompletionSpec;
---
+++ extensions/terminal-suggest/src/completions/codium.ts
@@ -0,0 +1,9 @@
+import code from './code';
+
+const codiumCompletionSpec: Fig.Spec = {
+	...code,
+	name: 'loophole',
+	description: 'Loophole',
+};
+
+export default codiumCompletionSpec;
