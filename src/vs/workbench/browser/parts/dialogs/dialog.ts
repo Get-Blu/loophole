16f56ad26cc33038de84b7bfba4aa03e3c4bf8c8
Garv Agnihotri <garvagnihotri22@gmail.com>
2026-07-19 10:44:15 +0530

    fix(version): show loopholeVersion in all UI dialogs and update notifications

--- src/vs/workbench/browser/parts/dialogs/dialog.ts
+++ src/vs/workbench/browser/parts/dialogs/dialog.ts
@@ -49,7 +49,7 @@ export function createBrowserAboutDialogDetails(productService: IProductService)
 	const detailString = (useAgo: boolean): string => {
 		return localize('aboutDetail',
 			"Loophole Version: {0}\nVS Code Version: {1}\nCommit: {2}\nDate: {3}\nBrowser: {4}",
-			productService.version || 'Unknown',
+			productService.loopholeVersion || productService.version || 'Unknown',
 			'1.121.0',
 			productService.commit || 'Unknown',
 			productService.date ? `${productService.date}${useAgo ? ' (' + fromNow(new Date(productService.date), true) + ')' : ''}` : 'Unknown',
@@ -66,4 +66,3 @@ export function createBrowserAboutDialogDetails(productService: IProductService)
 		detailsToCopy: detailsToCopy
 	};
 }
-
