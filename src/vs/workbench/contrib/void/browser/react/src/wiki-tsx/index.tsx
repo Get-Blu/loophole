e242ac1f20753b6a03abfbb620267406b0665e9c
Garv Agnihotri <garvagnihotri22@gmail.com>
2026-08-12 13:39:13 +0530

    Add index.tsx to mount SidebarWiki component

---
+++ src/vs/workbench/contrib/void/browser/react/src/wiki-tsx/index.tsx
@@ -0,0 +1,7 @@
+/*--------------------------------------------------------------------------------------
+ *  Copyright 2026 Garv Agnihotri, Inc. All rights reserved.
+ *--------------------------------------------------------------------------------------*/
+import { mountFnGenerator } from '../util/mountFnGenerator.js'
+import { SidebarWiki } from './SidebarWiki.js'
+
+export const mountWiki = mountFnGenerator(SidebarWiki)
